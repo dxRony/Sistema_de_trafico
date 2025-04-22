dd62807aef990b528d0ca4afe68c4e04cc03f76d
dxRony <romaroag@gmail.com>
2025-04-21 22:13:46 -0600

    creando repo

---
+++ src/main/java/com/mycompany/sistema_de_trafico/Sistema_de_trafico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_de_trafico;
+
+/**
+ *
+ * @author ronyrojas
+ */
+public class Sistema_de_trafico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!\nDesde EDD");
+    }
+}
